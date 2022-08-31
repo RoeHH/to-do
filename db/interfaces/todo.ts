60986c40550d9b1336e6bb264c7befbd9959449e
iccee0 <rouven@12i.ch>
2022-08-31 13:58:38 +0200

    uiiii

--- db/interfaces/todo.ts
+++ db/interfaces/todo.ts
@@ -1,4 +1,5 @@
 export interface Todo {
+    _id: string
     title: string;
     description: string;
     completed: boolean;
--- www/islands/Todo.tsx
+++ www/islands/Todo.tsx
@@ -13,9 +13,13 @@ export default function TodoField(props: CounterProps) {
   console.log(props.todo.completed);
   
   const [done, setDone] = useState(props.todo.completed);
+  const changeState = () => {
+    setDone(!done);
+    fetch("http://localhost:8000/api/changeTodoState/"+props.todo._id)
+  }
   return (
     <li class={done ? tw`font-light	opacity-100 line-through` : tw`font-medium hover:opacity-100`}>
-      <button onClick={() => setDone(!done)}>+</button>
+      <button onClick={changeState}>+</button>
       {done ? "t": "f"}{props.todo.title}
       </li>
   );
